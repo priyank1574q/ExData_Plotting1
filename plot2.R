7c1730c2de1b7bb43ad7b459357b689b13d755f5
Priyank <34156281+priyank1574q@users.noreply.github.com>
2018-07-02 00:59:01 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,10 @@
+x <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data <- x[x$Date %in% "2/2/2007" | x$Date %in% "1/2/2007",]
+data[,3:9] <- sapply(data[,3:9], function(f) as.numeric(as.character(f)))
+data$datetime <- apply(data[,1:2], 1, paste, collapse=" ") %>% strptime(format = "%d/%m/%Y %H:%M:%S")
+data <- data[,3:10]
+
+with(data, plot(datetime, Global_active_power, type = "l", lty = 1, main = "", xlab = "", ylab = "Global Active Power (kilowatts)"))
+
+dev.copy(png, file = "plot2.png")
+dev.off()
