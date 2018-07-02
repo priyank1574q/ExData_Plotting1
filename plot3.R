6df28d4844c4e6e309053d4dadf349e4fc7fd9d7
Priyank <34156281+priyank1574q@users.noreply.github.com>
2018-07-02 12:31:20 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,5 @@
 library(dplyr)
+
 x <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- x[x$Date %in% "2/2/2007" | x$Date %in% "1/2/2007",]
 data[,3:9] <- sapply(data[,3:9], function(f) as.numeric(as.character(f)))
@@ -10,5 +11,5 @@ with(data, lines(datetime, Sub_metering_2, col = "red", lty = 1))
 with(data, lines(datetime, Sub_metering_3, col = "blue", lty = 1))
 legend("topright", lty = 1, col = c("black","red","blue"), legend = c("Sub_m_1","Sub_m_2","Sub_m_3"))
 
-dev.copy(png, file = "plot3.png")
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
 dev.off()
