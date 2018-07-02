f8723574bb3414526f57ae0a8db779b1f6384307
Priyank <34156281+priyank1574q@users.noreply.github.com>
2018-07-02 12:30:14 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 library(dplyr)
+
 x <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 data <- x[x$Date %in% "2/2/2007" | x$Date %in% "1/2/2007",]
 data[,3:9] <- sapply(data[,3:9], function(f) as.numeric(as.character(f)))
@@ -7,5 +8,5 @@ data <- data[,3:10]
 
 with(data, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
 
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
